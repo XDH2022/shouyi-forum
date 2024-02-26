6282438bde18eabfe02a78c41e990fda476a8a45
86152 <xdh2201@gmail.com>
2024-02-26 15:09:02 +0800

    裁剪图片bug修复

--- shouyi-admin/src/views/userInfo.vue
+++ shouyi-admin/src/views/userInfo.vue
@@ -140,10 +140,35 @@
           </el-form>
         </el-card>
       </el-col>
+      <el-col :span="8">
+            <div class="box-card">
+              <div style="font-size: 16px"> 🎅🏽 关于本站</div>
+              <br/>
+              <div style="color:#00000073">一个充满着青春气息，活力满满的校园交流平台</div>
+              <br>
+              <div style="color:#00000073">这里是你的校友之家，
+                无论你毕业多久，无论你身在何处，
+                你都可以在这里找到你的同窗好友，
+                分享你的学习和生活经历，参与各种有趣的活动，获取最新的校园资讯，为母校代言，为校友服务。</div>
+              <br>
+              <div style="color:#00000073">这里是你的校友市场，
+                无论你是卖家，买家，还是观望者，
+                你都可以在这里利用校友的信任，
+                发布或浏览二手商品，进行安全便捷的交易，节省你的开支，增加你的收入，实现你的物品再利用。</div>
+              <br>
+              <div style="color:#00000073">这里是你的校友社区，
+                无论你是文艺青年，运动达人，还是科技发烧友，
+                你都可以在这里找到志同道合的校友，
+                一起交流你的兴趣爱好，一起探索你的知识领域，一起享受你的精彩时光。</div>
+              <br>
+              <div style="color:#00000073">欢迎你加入我们，让我们一起为校友，为母校，为社会，贡献我们的一份力量！</div>
+            </div>
+      </el-col>
     </el-row>
     <el-dialog title="裁剪图片" v-model="dialogVisible" width="600px">
       <vue-cropper
           ref="cropper"
+          :key="cropperKey"
           :src="updateUserReq.user.avatar"
           :ready="cropImage"
           :zoom="cropImage"
@@ -250,16 +275,76 @@ const setImage = async (info: any) => {
   const formData = new FormData();
   formData.append('file', file);
   const result = await uploadOssImg(formData)
+
+
   if (result.code == 0) {
-    updateUserReq.user.avatar = result.data
-    ElMessage.success('上传头像成功')
-  } else {
+    updateUserReq.user.avatar=result.data
+    console.log( updateUserReq.user.avatar);
+    ElMessage.success("上传头像成功");
+    cropperKey.value = Date.now();
+  }
+  else {
     ElMessage.error(result.message)
   }
 }
-const saveAvatar = (id: number, email: string) => {
-  updateOnSubmit(id, email)
+
+const cropperKey = ref(Date.now());
+// 用 import 导入 blob-util 模块
+import { dataURLToBlob } from 'blob-util';
+
+// 定义一个函数，将 dataURL 转换为 Blob 对象
+function dataURLtoBlob(dataUrl) {
+  // 将 dataUrl 分割成两部分
+  let arr = dataUrl.split(',');
+  // 获取媒体类型
+  let mime = arr[0].match(/:(.*?);/)[1];
+  // 获取 base64 编码的数据
+  let bstr = atob(arr[1]);
+  // 创建一个 Uint8Array 对象
+  let n = bstr.length;
+  let u8arr = new Uint8Array(n);
+  // 将 base64 编码的数据解码成 Uint8Array 对象
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  // 将 Uint8Array 对象转换成 Blob 对象
+  let blob = new Blob([u8arr], {type: mime});
+  // 返回 Blob 对象
+  return blob;
 }
+const saveAvatar = async (id: number, email) => {
+  if (cropImg.value) {
+    let blob = dataURLtoBlob(cropImg.value);
+    const formData = new FormData();
+    // 这里假定cropImg.value是一个base64编码的图像数据，我们需要将其转换成Blob对象
+    formData.append('file', blob);
+    try {
+      const result = await uploadOssImg(formData); // 替换为您上传裁剪图像的API调用
+      if (result.code == 0) {
+        ElMessage.success("头像更新成功");
+        // 在用户存储或必要的地方更新用户的头像
+        updateUserReq.user.avatar = result.data
+        await updateOnSubmit(id,userStore.currentUser.email)
+      } else {
+        ElMessage.error(result.message);
+      }
+    } catch (error) {
+      ElMessage.error("上传失败");
+      console.error(error);
+    }
+  } else {
+    ElMessage.warning("请先裁剪图片");
+  }
+};
+const cropImg = ref('');
+const cropper: any = ref();
+const cropImage = () => {
+  if (cropper.value && cropper.value.getCroppedCanvas()) {
+    cropImg.value = cropper.value.getCroppedCanvas().toDataURL();
+  } else {
+    console.error('裁剪框还没有准备好');
+  }
+};
 const options = [
   {
     value: 1,
@@ -283,18 +368,13 @@ const sexOptions = [
 ]
 
 
-const cropImg = ref('');
-const cropper: any = ref();
-const cropImage = () => {
-  cropImg.value = cropper.value.getCroppedCanvas().toDataURL();
-};
+
 </script>
 
 <style scoped>
 .box-card {
-  width: 980px;
+  width: 620px;
   margin-top: 15px;
-
   color: #00000073;
 }
 
